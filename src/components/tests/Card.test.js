8a529e1d4f38e33b51837496bb8a448365bbe363
Rafael <perchespierri@gmail.com>
2022-03-11 16:34:02 -0300

    Refactor: Card testing now is smaller with same efficiency

--- src/components/tests/Card.test.js
+++ src/components/tests/Card.test.js
@@ -1,36 +1,20 @@
 import { render } from '@testing-library/react'
 import Card from '../Card';
 
-
 describe('Card component', () => {
-  
-  it('Renders card of spades correctly', () => {
-    const testCard = { value: 'A', suit: 'spades' };
-    const { container } = render(<Card card={ testCard } />);
-    expect(container).toMatchSnapshot();
-  });
 
-  it('Renders card of hearts correctly', () => {
-    const testCard = { value: 'A', suit: 'hearts' };
-    const { container } = render(<Card card={ testCard } />);
-    expect(container).toMatchSnapshot();
-  });
+  it.each(['hearts', 'spades', 'diamonds', 'clubs'])
+    (`Renders correctly when suit is %s`, (suit) => {    
+      const testCard = { value: 'A', suit: suit };
+      const { container } = render(<Card card={ testCard } />);
+      
+      expect(container).toMatchSnapshot();
+    });
 
-  it('Renders card of diamonds correctly', () => {
-    const testCard = { value: 'A', suit: 'diamonds' };
-    const { container } = render(<Card card={ testCard } />);
-    expect(container).toMatchSnapshot();
-  });
-
-  it('Renders card clubs correctly', () => {
-    const testCard = { value: 'A', suit: 'clubs' };
-    const { container } = render(<Card card={ testCard } />);
-    expect(container).toMatchSnapshot();
-  });
-
-  it('Renders card of any value correctly', () => {
-    const testCard = { value: 'TEST VALUE', suit: 'clubs' };
-    const { container } = render(<Card card={ testCard } />);
-    expect(container).toMatchSnapshot();
-  });
+    it('Renders card of any value correctly', () => {
+      const testCard = { value: 'TEST VALUE', suit: 'clubs' };
+      const { container } = render(<Card card={ testCard } />);
+      
+      expect(container).toMatchSnapshot();
+    });
 })
