f674244b393dd1b3fb8f74455f0b1dbbb82967ac
Rafael <perchespierri@gmail.com>
2022-03-11 16:55:27 -0300

    Impl> Stack tests

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,9 +3,9 @@ import '../styles/Card.css';
 
 export default function Card({ card: {value, suit} }) { 
   return (
-    <>
+    <div role="img" aria-label={`Image of the ${value} of ${suit} card`} >
       {value}
       <div className={`Card__suit--image Card__suit--${suit}`} />
-    </>
+    </div>
   )
 }
